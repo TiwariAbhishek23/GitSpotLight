26b5c41d20e24c520fae321473327e184da36e3b
Abhishek Tiwari <tiwari.abhishektiwari23@gmail.com>
2023-05-20 02:45:54 +0530

    Removed unused files and updated the api files

---
+++ pages/api/repoapi.js
@@ -0,0 +1,14 @@
+export default async function handler(req, res) {
+    const { username } = req.query;
+  
+    try {
+      const response = await fetch(`https://api.github.com/users/${username}/repos`);
+      if (!response.ok) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+      const data = await response.json();
+      res.status(200).json(data);
+    } catch (error) {
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  }
