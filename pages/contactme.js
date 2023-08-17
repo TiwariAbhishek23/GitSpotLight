0f934c17d0a32b0ca3277137404d05f3a50cd03f
Abhishek Tiwari <tiwari.abhishektiwari23@gmail.com>
2023-08-17 20:44:46 +0530

    fastapi setup

--- pages/contactme.js
+++ pages/contactme.js
@@ -8,11 +8,11 @@ const Contactme = () =>{
         <div className="wrap flex">
         <div className="discription lg:inline hidden w-1/3 p-8 m-8 text-white">
             
-           <div className="text-4xl"> Hi <span className='wave '>👋</span>, <span className='grade'>{" <Geek/> "}</span></div> <br/>Feel free to talk to me about anything that bothers you. If you need a helping hand, please don't hesitate to contact me. Got a question on the services that I provide? I am just a click away.
+           <div className="text-4xl"> Hi <span className='wave '>👋</span>, <span className='grade'>{" <Geek/> "}</span></div> <br/>Feel free to discuss anything that bothers you. 😊 If you need help, don&apos;t hesitate to contact me. 📞 Do you have questions about my services? 💬 I&apos;m just a click away. 👀
             </div>
         <ContactmeCont className="folat-right inline-block"/>
         
-        
+
         </div>
         <div className='notice-para font-extralight lg:font-medium text-xs '>Field Required <span className="notice">*</span></div>
         </div>
