a418b55e4145389ad604ed2673992975022a2c1b
Abhishek Tiwari <tiwari.abhishektiwari23@gmail.com>
2023-08-20 17:04:40 +0530

    api route configuration

---
+++ backend/api/routes/route_user.py
@@ -0,0 +1,7 @@
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from datetime import timedelta
+from core.config import settings
+
+user_router = APIRouter()
--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,32 @@
 from fastapi import FastAPI
+from core.config import settings
+from database.sessions import engine, SessionLocal
+from database.baseclass import Base
 
-app = FastAPI()
+from api.routes import route_user, route_company, route_auth, route_email
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+
+def include_router(app):
+    app.include_router(route_user.user_router, tags=["Users"], prefix="/users")
+    app.include_router(route_company.company_router, tags=["Company"], prefix="/company")
+    app.include_router(route_auth.login_router, tags=["Auth"], prefix="/auth")
+    app.include_router(route_email.email_router, tags=["Contact"], prefix="/contact")
+
+def configure_database(app):
+    Base.metadata.create_all(bind=engine)
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
+def start_application():
+    app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
+    include_router(app)
+    configure_database(app)
+    return app
+
+app = start_application()
